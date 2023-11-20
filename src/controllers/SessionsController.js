ddfd9a5cfb26b67bb7c5386e52123007215434af
rafael <rafael.penner465@gmail.com>
2023-11-20 17:54:44 -0300

    cookies

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -25,7 +25,16 @@ class SessionsController {
         const token = sign({ role: user.role }, secret, {
             subject: String(user.id),
             expiresIn
-        })        
+        });
+
+        response.cookie("token", token), {
+            httpOnly: true,
+            sameSite: "none",
+            secure: true,
+            maxAge: 15 * 60 * 1000
+        }
+
+        delete user.password
 
         return response.json({ user, token });
     }
