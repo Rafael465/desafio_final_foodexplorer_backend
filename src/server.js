9daec8dbf5f92443663ae8671074fd96eb00fba9
rafael <rafael.penner465@gmail.com>
2023-12-04 03:40:29 -0300

    origin update

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://foodexplorfrontend.netlify.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
