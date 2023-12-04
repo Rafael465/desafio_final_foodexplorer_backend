1521caa22708f5ec93688631fa6e4b8c7012142f
rafael <rafael.penner465@gmail.com>
2023-12-04 03:24:41 -0300

    cors update

--- src/server.js
+++ src/server.js
@@ -16,7 +16,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
-    credentials: true
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
